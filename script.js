392b8bac14ddc135f323170ecd5da4eeba70607a
drsaraths <saraths@am.amrita.edu>
2026-09-20 16:19:07 +0530

    Set email user and domain for contact form

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 // Fill in both parts of your address, for example USER = "name" and DOMAIN = "am.amrita.edu".
 // The address is joined only when a visitor clicks "Send me an email". While either part
 // is empty, the email line stays hidden.
-const EMAIL_USER = "";
-const EMAIL_DOMAIN = "";
+const EMAIL_USER = "saraths";
+const EMAIL_DOMAIN = "am.amrita.edu";
 
 document.querySelectorAll("[data-current-year]").forEach((el) => {
   el.textContent = new Date().getFullYear();
